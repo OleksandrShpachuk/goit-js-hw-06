5a03ab122884818c32e798a3d83fd438b71b68d9
OleksandrShpachuk <al.shpachuk@gmail.com>
2023-05-19 11:11:40 +0300

    Done

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", submit);
 
+function submit(event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+    return;
+  }
+  const loginData = { email: emailInput.value, password: passwordInput.value };
+
+  console.log(loginData);
+  loginForm.reset();
+}
